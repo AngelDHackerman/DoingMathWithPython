ad893e296372ee8e6d1d6835973662af60e0ca2f
Angel Hernandez <angeldariaux@gmail.com>
2024-06-11 21:30:23 -0600

    finding multiple modes

--- Chap_3_Describing_Data_With_Statistics/find_multiple_modes.py
+++ Chap_3_Describing_Data_With_Statistics/find_multiple_modes.py
@@ -1,17 +1,24 @@
 
-# Calculating the modes
+# Calculating the modes when the list of numbers may
+# hve multiple modes
 
 from collections import Counter
 
 def calculate_mode(numbers):
   c = Counter(numbers)
-  mode = c.most_common(1)
-  mode, frequency = mode[0][0], mode[0][1]
-  return mode, frequency
+  numbers_freq = c.most_common()
+  max_count = numbers_freq[0][1]
+
+  modes = []
+  for num in numbers_freq:
+    if num[1] == max_count:
+      modes.append(num[0])
+  return modes
 
 
 if __name__ == "__main__":
-  scores = [7, 8, 9, 2, 10, 9, 9, 9, 4, 5, 6, 7, 8, 6, 1, 10]
-  mode, frequency = calculate_mode(scores)
-
-  print('The mode of the list of numbers is: {0}, it appears {1} times'.format(mode, frequency))
+  scores = [5, 5, 5, 4, 4, 4, 9, 1, 3]
+  modes = calculate_mode(scores)
+  print('The mode(s) of the list of numbers are: ')
+  for mode in modes:
+    print(mode)
