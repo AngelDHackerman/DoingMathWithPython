87a4ec56ddb0f1396a7d50198d58a2d40485ec26
Angel Hernandez <angeldariaux@gmail.com>
2024-07-05 20:24:15 -0600

    agregadas notas en el codigo

--- curso_estadistica_probabilidad_platzi/altura_personas_estadistica_descriptiva.py
+++ curso_estadistica_probabilidad_platzi/altura_personas_estadistica_descriptiva.py
@@ -3,8 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Generando datos de ejemplo
-np.random.seed(42)
-heights = np.random.normal(loc=170, scale=10, size=100)
+np.random.seed(42) # crea un punto de partida para los numeros aleatorios, haciendo que estos sean reproducibles.
+heights = np.random.normal(loc=170, scale=10, size=100) # genera numeros aleatorios siguiendo una distribucion normal (o distribucion gaussiana)
+  # loc=170: la media (o promedio) de la distribucion)
+  # scale=10: la desviacion estandar de la distribucion
+  # size=100: el numero de muestras a generar
+  # Esta línea genera 100 valores de altura que están distribuidos normalmente alrededor de una media de 170 cm con una desviación estándar de 10 cm.
 
 # Cálculos de estadística descriptiva
 mean_height = np.mean(heights)
--- curso_estadistica_probabilidad_platzi/ingresos_personas_estadistica_inferencial.py
+++ curso_estadistica_probabilidad_platzi/ingresos_personas_estadistica_inferencial.py
@@ -8,11 +8,11 @@ np.random.seed(42)
 sample_income = np.random.normal(loc=50000, scale=15000, size=100)
 
 # Intervalo de confianza para la media
-confidence_level = 0.95
-degrees_freedom = len(sample_income) - 1
-sample_mean = np.mean(sample_income)
-sample_std_err = stats.sem(sample_income)
-confidence_interval = stats.t.interval(confidence_level, degrees_freedom, sample_mean, sample_std_err)
+confidence_level = 0.95 # define el nivel de confianza para el intervalo de confianza. Un nivel de confianza del 95% significa que, si repetimos este proceso muchas veces, el 95% de los intervalos de confianza calculados contendrá la media verdadera de la población.
+degrees_freedom = len(sample_income) - 1 # Los grados de libertad se calculan como el tamaño de la muestra menos uno (n - 1). Se utilizan en la distribución t de Student, que es apropiada para muestras pequeñas.
+sample_mean = np.mean(sample_income) 
+sample_std_err = stats.sem(sample_income) # calcula el error estándar de la media de la muestra. El error estándar mide la precisión con la que la media muestral estima la media de la población.
+confidence_interval = stats.t.interval(confidence_level, degrees_freedom, sample_mean, sample_std_err) # calcula el intervalo de confianza para la media poblacional usando la distribución t de Student.
 
 print(f"Sample Mean Income: {sample_mean}")
 print(f"95% Confidence Interval for Mean Income: {confidence_interval}")
